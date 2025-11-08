e12296230a93da743d8da5ac216aac46f5a3c717
Raider_Dude <raider_dude100@hotmail.com>
2025-11-08 10:10:27 +0300

    Fixed installer script

--- Scripts/install.sh
+++ Scripts/install.sh
@@ -1,60 +1,26 @@
 #!/bin/sh
 
-PATH="~/dotfiles"
-if [ -d "$FOLDER_PATH" ]; then
-  echo "Already downloaded"
-  cd Install
-  chmod +x *
-  ./install-yay.sh
-  ./install-chaotic-aur.sh
-  ./install-themeing.sh
-  ./install-utilities.sh
+cd Install
+chmod +x *
+./install-yay.sh
+./install-chaotic-aur.sh
+./install-themeing.sh
+./install-utilities.sh
 
-  cd ..
-  cd ..
-  stow btop fastfetch gamemode gtk-2 gtk-3 gtk-4 hypr kitty matugen nwg-look qt5ct qt6ct rofi swaync vim wallust waybar zed zsh
+cd ..
+cd ..
+stow btop fastfetch gamemode gtk-2 gtk-3 gtk-4 hypr kitty matugen nwg-look qt5ct qt6ct rofi swaync vim wallust waybar zed zsh
 
-  cd Scripts
-  cd Install
-  ./install-programs.sh
+cd Scripts
+cd Install
+./install-programs.sh
 
-  cd ..
-  cd Theme
-  chmod +x theme.sh
-  ./theme.sh
+cd ..
+cd Theme
+chmod +x theme.sh
+./theme.sh
 
-  cd Post
-  chmod +x *
-  ./post.sh
-
-  
-else
-  echo "Downloading from github"
-  git clone https://github.com/RaiderDude100/dotfiles.git
-  cd dotfiles
-  cd Install
-  chmod +x *
-  ./install-yay.sh
-  ./install-chaotic-aur.sh
-  ./install-themeing.sh
-  ./install-utilities.sh
-
-  cd ..
-  cd ..
-  stow btop fastfetch gamemode gtk-2 gtk-3 gtk-4 hypr kitty matugen nwg-look qt5ct qt6ct rofi swaync vim wallust waybar zed zsh
-
-  cd Scripts
-  cd Install
-  ./install-programs.sh
-
-  cd ..
-  cd Theme
-  chmod +x theme.sh
-  ./theme.sh
-
-  cd Post
-  chmod +x *
-  ./post.sh
-  
-fi
+cd Post
+chmod +x *
+./post.sh
 
--- installer.sh
+++ installer.sh
@@ -1,8 +1,18 @@
 #!/bin/sh
 
-cd ~
-git clone https://github.com/RaiderDude100/dotfiles.git
-cd dotfiles/Scripts
-chmod +x install.sh
-./install.sh
+PATH="~/dotfiles"
+if [ -d "$FOLDER_PATH" ]; then
+  echo "Already downloaded"
+  cd Scripts
+  ./install.sh
+
+  
+else
+  echo "Downloading from github"
+  git clone https://github.com/RaiderDude100/dotfiles.git
+  cd dotfiles
+  cd Scripts
+  ./install.sh
+  
+fi
 
