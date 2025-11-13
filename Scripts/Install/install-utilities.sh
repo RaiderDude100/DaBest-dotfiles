ed131400a5dcaf2c742b5a150da247cb75027f98
Raider_Dude <raider_dude100@hotmail.com>
2025-11-13 21:47:09 +0300

    More wallpapers and fixes

--- Scripts/Install/install-utilities.sh
+++ Scripts/Install/install-utilities.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
-yay -S polkit polkit-gnome polkit-qt6 7zip android-file-transfer android-udev aquamarine auto-cpufreq bat blueman bluez bluez-libs bluez-obex bluez-utils brightnessctl btop eza gamemode lib32-gamemode grim gtk gtk2 gtk3 gtk4 imagemagick ly ncdu ntfs-3g pacseek pavucontrol qdiskinfo-bin stow tumbler zoxide zsh reflector resources --needed --noconfirm
+yay -S polkit polkit-gnome polkit-qt6 7zip android-file-transfer flatpak android-udev aquamarine auto-cpufreq bat blueman bluez bluez-libs bluez-obex bluez-utils brightnessctl btop eza gamemode lib32-gamemode grim imagemagick ly ncdu ntfs-3g pacseek pavucontrol qdiskinfo-bin stow tumbler zoxide zsh reflector resources gtk gtk2 gtk3 gtk4 xdg-desktop-portal xdg-desktop-portal-hyprland xdg-desktop-portal-gtk xdg-user-dirs xdg-utils xdg-dbus-proxy --needed --noconfirm
 
 sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
 echo "Installed utilities"
